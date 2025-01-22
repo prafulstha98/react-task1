10aafc37105503bd81ccf28d9ccc7a4fb295fb31
kali <kali@kali>
2025-01-22 20:14:02 +0545

    Routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import RootLayout from './components/RootLayout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import { RouterProvider } from 'react-router'
 
 
 export const App = () => {
-  return (
-    <div>App
-
-    </div>
-
-    
-  )
+  const routerFuture = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout/>,
+      children: [
+        {
+          index: true,
+          element: <Home/>,
+        },
+        {
+          path: '/about',
+          element: <About/>,
+        },
+        {
+          path: '/contact',
+          element: <Contact/>,
+        },
+      ]
+    },
+    {},
+  ])
+  return <RouterProvider router={routerFuture}/> 
 }
--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,6 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
 const RootLayout = () => {
+
   return (
-    <div>RootLayout</div>
+    <div>
+      {/* this file includes 
+      header, footer, hero section */}
+  <Navbar/>  
+  <Outlet/>
+  <Footer/>
+    </div>
   )
 }
 export default RootLayout
