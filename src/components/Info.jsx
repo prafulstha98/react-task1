de7eb243d342b6a2c6a1df0192e9eb3f5cda62ec
kali <kali@kali>
2025-01-22 22:59:21 +0545

    Completed Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const Info = () => {
   return (
     <div>
-      
+
+      <h1 className='text-4xl font-bold text-center'>Who am i</h1>
+
+<div className='flex justify-center'>
+
+<div className='bg-black w-[300px] h-[300px] rounded-full border-green-600 border-4 text-white flex items-center justify-center mt-10'>
+
+  <div>
+<p>Name:Ben 10</p>
+<p>Email: info@bentension.com</p>
+<p>Tel: 9578575</p>
+<p>Age: 10</p>
+<p>Experience: 10 Aliens</p>
+  </div>
+
+</div>
+
+</div>
+
     </div>
   )
 }
