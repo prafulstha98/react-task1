9b6fee5fd50725f3fae29f06d57403b888cf4413
kali <kali@kali>
2025-01-22 22:44:26 +0545

    completed Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { FaReact,FaPython } from "react-icons/fa6";
+import { FaJava,FaCss3 } from "react-icons/fa"; 
+import { SiHtml5,SiKotlin } from "react-icons/si";
+
 
 const Tech = () => {
   return (
     <div>
-      
+
+    <h1 className='text-4xl text-center font-bold'>Technologies I use</h1>
+
+    <div className='grid grid-cols-4 gap-y-20 my-10 justify-items-center'>
+<FaReact size={125}/>
+<FaJava size={125}/>
+<SiHtml5  size={125}/>
+<FaPython  size={125}/>
+<FaCss3  size={125}/>
+<SiKotlin  size={125}/>
+    </div>
+
     </div>
   )
 }
